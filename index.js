d8be0d1f50a5bca59f2b487ac4cfaa546087070e
unknown <kylespinaio@hotmail.com>
2020-07-14 22:46:51 -0400

    added repository and data folders added repo context

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-const express = require('express'); 
-const app = express();
+const repoContext = require("./repository/repository-wrapper.js");
 
-app.listen(3000, function(){
-    console.log("Server started. Listening on port 3000.");
-
-});
-
-=======
 const express = require('express');
 
 const app = express();
@@ -15,4 +7,3 @@ const app = express();
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
    });
->>>>>>> 559152f81559e7f21998fdccb384d203d39b3393
