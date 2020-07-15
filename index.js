a66c5c1ab2cfe93fb09478aa65bd8a762400383f
unknown <kylespinaio@hotmail.com>
2020-07-14 22:49:52 -0400

    pushing saved copy

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const repoContext = require("./repository/repository-wrapper.js");
 
 const express = require('express');
@@ -8,14 +7,3 @@ const app = express();
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
    });
-=======
-
-const express = require('express'); 
-const app = express();
-
-app.listen(3000, function(){
-    console.log("Server started. Listening on port 3000.");
-
-});
-
->>>>>>> 695de87dea5603ede5ac0b1da85f7a386354c366
