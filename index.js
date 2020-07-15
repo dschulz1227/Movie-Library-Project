2ae3ce4c02fba1858e0821af6f390f607b7a64a7
unknown <kylespinaio@hotmail.com>
2020-07-15 19:19:39 -0400

    added cors to index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cors = require("cors");
 const validators = require("./validators/custom-validations.js");
 
 const repoContext = require("./repository/repository-wrapper.js");
@@ -5,6 +6,7 @@ const repoContext = require("./repository/repository-wrapper.js");
 const express = require('express');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => validators.body(req, res, next));
