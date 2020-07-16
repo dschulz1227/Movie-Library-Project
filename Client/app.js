df5306133645224fab00f3a76181036d84655166
Damon Schulz <damon.schulz@gmail.com>
2020-07-15 20:55:32 -0500

    Created getAllMovies function,createMovie function, and getMovieObject function

--- Client/app.js
+++ Client/app.js
@@ -1,27 +1,57 @@
 "use strict"
 
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajax({
-        crossDomain:true,
-        contentType:"application/json",
-        url:"http://localhost:3000/api/movies",
-        dataType:"json",
+        crossDomain: true,
+        contentType: "application/json",
+        url: "http://localhost:3000/api/movies",
+        dataType: "json",
         type: "get",
-        
-        success: function(data, second, third){
-            // let result = JSON.stringify(data)
-            for (let i = 0; i < data.length; i++){
-                    $("#movieTable").find('tbody').append(`<tr><td>${data[i]["id"]}</td><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td></tr>`)
-                    
 
-                }
+        success: function (data, second, third) {
+            // let result = JSON.stringify(data)
+            for (let i = 0; i < data.length; i++) {
+                $("#movieTable").find('tbody').append(`<tr><td>${data[i]["id"]}</td><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td></tr>`)
+
+
+            }
         },
-        error: function(meta, errorThrown, third){
+        error: function (meta, errorThrown, third) {
             console.log(errorThrown);
         }
-
-
-
-
     })
 })
+
+function getAllMovies(){
+    $.ajax({
+        crossDomain: true,
+        contentType: "application/json",
+        url: "http://localhost:3000/api/movies",
+        dataType: "json",
+        type: "get"
+    }
+    )}
+
+function getMovieObject(){
+    var data = {
+    "id": document.getElementById('id').value,
+    "title": document.getElementById('title').value,
+    "genre": document.getElementById('genre').value,
+    "director":document.getElementById('director').value
+    }
+}
+
+function createMovie(){
+    var data = getMovieObject();
+    $(document).ready(function(){
+        $.ajax({
+            type:'POST',
+            url:"http://localhost:3000/api/movies",
+            data:data
+        }).then(function(){
+            getAllMovies();
+        })
+    })
+
+    
+}
