680ad8dbc338c384792aa43a400e1065df2eb697
Damon Schulz <damon.schulz@gmail.com>
2020-07-15 21:53:23 -0500

    Commented out button functions

--- Client/app.js
+++ Client/app.js
@@ -22,36 +22,52 @@ $(document).ready(function () {
     })
 })
 
-function getAllMovies(){
-    $.ajax({
-        crossDomain: true,
-        contentType: "application/json",
-        url: "http://localhost:3000/api/movies",
-        dataType: "json",
-        type: "get"
-    }
-    )}
+// function getAllMovies(){
+//     $.ajax({
+//         crossDomain: true,
+//         contentType: "application/json",
+//         url: "http://localhost:3000/api/movies",
+//         dataType: "json",
+//         type: "get"
+//     }
+//     )}
 
-function getMovieObject(){
-    var data = {
-    "id": document.getElementById('id').value,
-    "title": document.getElementById('title').value,
-    "genre": document.getElementById('genre').value,
-    "director":document.getElementById('director').value
-    }
+// function getMovieObject(){   
+//      $.ajax({
+//         crossDomain: true,
+//         contentType: "application/json",
+//         url: "http://localhost:3000/api/movies",
+//         dataType: "json",
+//         type: "get",
+
+//         success: function (data, second, third) {
+//             // let result = JSON.stringify(data)
+//             // for (let i = 0; i < data.length; i++) {
+//              $("#movieTable").find('tbody').append(`<tr><td>${data[data.length]["id"]}</td><td>${data[data.length]["title"]}</td><td>${data[data.length]["genre"]}</td><td>${data[data.length]["director"]}</td></tr>`)
+
+
+            // }
+        // $("#movieTable").find('tbody').append(`<tr><td>${data[i]["id"]}</td><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td></tr>`)
+    // "id": document.getElementById('id').value,
+    // "title": document.getElementById('title').value,
+    // "genre": document.getElementById('genre').value,
+    // "director":document.getElementById('director').value
+        }
+    })
 }
 
-function createMovie(){
-    var data = getMovieObject();
-    $(document).ready(function(){
-        $.ajax({
-            type:'POST',
-            url:"http://localhost:3000/api/movies",
-            data:data
-        }).then(function(){
-            getAllMovies();
-        })
-    })
+
+// function createMovie(){
+//     var data = getMovieObject();
+//     $(document).ready(function(){
+//         $.ajax({
+//             type:'POST',
+//             url:"http://localhost:3000/api/movies",
+//             data:data
+//         }).then(function(){
+//             getAllMovies();
+//         })
+//     })
 
     
 }
